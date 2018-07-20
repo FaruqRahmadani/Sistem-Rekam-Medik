37939cb3d436d3e2b3480c92585aba900617a054
FaruqRahmadani <faruq.rahmadani@yahoo.com>
2018-07-21 01:32:57 +0800

    view Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::group(['middleware' => ['AuthMiddleware']], function () {
-  Route::get('', function () {
-    return view('Blank');
-  });
-});
-
 Route::GET('login', 'Auth\LoginController@showLoginForm')->name('LoginForm');
 Route::POST('login', 'Auth\LoginController@login')->name('Login');
 Route::GET('logout', 'Auth\LoginController@logout')->name('Logout');
+
+Route::group(['middleware' => ['AuthMiddleware']], function () {
+  Route::GET('', 'DashboardController@Dashboard');
+});
+
 
 Route::get('/home', 'HomeController@index')->name('home');
